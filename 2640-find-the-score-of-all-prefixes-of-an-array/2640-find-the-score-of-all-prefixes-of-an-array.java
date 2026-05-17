3ffc9f77018a116f45932c6ce230928a888df602
yashpal parmar <144250326+yashpal8078@users.noreply.github.com>
2026-05-17 23:15:55 +0530

    Time: 6 ms (1.72%), Space: 95 MB (45.27%) - LeetHub

--- 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.java
+++ 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.java
@@ -2,14 +2,13 @@ class Solution {
     public long[] findPrefixScore(int[] nums) {
 
         long[] conversion = new long[nums.length];
-        int max = 0;
-        for(int i  = 0;i<nums.length;i++){
+        
+        int max = nums[0];
+        conversion[0] = nums[0]+max;
+        for(int i  = 1;i<nums.length;i++){
              max  = Math.max(max,nums[i]);
              conversion[i]  = nums[i] + max;
-        }
-
-        for(int i = 1;i<conversion.length;i++){
-            conversion[i]  = conversion[i-1] + conversion[i];
+             conversion[i]  = conversion[i-1] + conversion[i];
         }
 
         return conversion;
