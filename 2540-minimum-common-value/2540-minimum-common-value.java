272093d1b1aad06d3f88e2031becc786e0c07207
yashpal parmar <144250326+yashpal8078@users.noreply.github.com>
2026-05-19 07:54:32 +0530

    Time: 15 ms (15.62%), Space: 96.8 MB (5.73%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+         
+
+         HashMap<Integer,Integer> map = new HashMap<>();
+
+         for(int i = 0;i<nums1.length;i++){
+            map.put(nums1[i] , i);
+         }
+         for(int i = 0;i<nums2.length;i++){
+            if(map.containsKey(nums2[i])){
+                return nums2[i];
+            }
+         }
+
+        return -1;
+    }
+}
