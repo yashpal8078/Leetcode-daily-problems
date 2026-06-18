ccbecf1720ee570f476a19ce5bc6457e9b482726
yashpal parmar <144250326+yashpal8078@users.noreply.github.com>
2026-06-18 23:57:30 +0530

    Time: 1 ms (99.97%), Space: 46.5 MB (58.31%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,19 +1,43 @@
 class Solution {
-    public boolean isValidSudoku(char[][] board) {
 
-        HashSet<String> set = new HashSet<>();
+    boolean isValid(char[][] board, int row, int col, char d) {
+
+        for (int i = 0; i < 9; i++) {
+
+            if (board[row][i] == d)
+                return false;
+
+            if (board[i][col] == d)
+                return false;
+        }
+
+        int start_i = (row / 3) * 3;
+        int start_j = (col / 3) * 3;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[start_i + i][start_j + j] == d)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isValidSudoku(char[][] board) {
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
 
-                char ch = board[i][j];
+                if (board[i][j] != '.') {
 
-                if (ch == '.') continue;
+                    char ch = board[i][j];
+                    board[i][j] = '.';      
 
-                if (!set.add(ch + " in row " + i) ||
-                    !set.add(ch + " in col " + j) ||
-                    !set.add(ch + " in box " + (i / 3) + "-" + (j / 3))) {
-                    return false;
+                    if (!isValid(board, i, j, ch))
+                        return false;
+
+                    board[i][j] = ch;       
                 }
             }
         }
